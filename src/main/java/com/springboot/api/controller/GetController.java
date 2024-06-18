11cd20bff7c53c2711271e5996d2977f1428af4e
hayannn <dlgkdis801@naver.com>
2024-06-18 21:42:51 +0900

    feat: @PathVariable에 변수명을 매핑하는 방법

--- src/main/java/com/springboot/api/controller/GetController.java
+++ src/main/java/com/springboot/api/controller/GetController.java
@@ -19,9 +19,16 @@ public class GetController {
     }
      */
 
-    // @PathVariable을 활용한 GET 메서드 구현 -> {String 값}
+    /* @PathVariable을 활용한 GET 메서드 구현 -> {String 값}
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable1(@PathVariable String variable){
         return variable;
+    }
+     */
+
+    // @PathVariable에 변수명을 매핑하는 방법
+    @GetMapping(value = "/variable2/{variable}")
+    public String getVariable2(@PathVariable("variable") String var){
+        return var;
     }
 }
