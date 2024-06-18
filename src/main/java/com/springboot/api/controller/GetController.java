b16aa34f4efece93ab51d2670a3e1cf11140b9f2
hayannn <dlgkdis801@naver.com>
2024-06-18 22:00:59 +0900

    style: 세미콜론 제거

--- src/main/java/com/springboot/api/controller/GetController.java
+++ src/main/java/com/springboot/api/controller/GetController.java
@@ -10,33 +10,30 @@ import java.util.Map;
 @RequestMapping("/api/v1/get-api")
 public class GetController {
 
-    /*@RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String getHello() {
         return "Hello World";
-    }*/
+    }
 
-    /*매개 변수가 없는 GET 메서드 구현
+    // 매개 변수가 없는 GET 메서드 구현
     @GetMapping(value = "/name")
     public String getName(){
         return "Flature";
     }
-     */
 
-    /* @PathVariable을 활용한 GET 메서드 구현 -> {String 값}
+    // @PathVariable을 활용한 GET 메서드 구현 -> {String 값}
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable1(@PathVariable String variable){
         return variable;
     }
-     */
 
-    /* @PathVariable에 변수명을 매핑하는 방법
+    // @PathVariable에 변수명을 매핑하는 방법
     @GetMapping(value = "/variable2/{variable}")
     public String getVariable2(@PathVariable("variable") String var){
         return var;
     }
-     */
 
-    /*@RequestParam을 활용한 GET 메서드 구현
+    // @RequestParam을 활용한 GET 메서드 구현
     @GetMapping(value = "/request1")
     public String getRequestParam1(
             @RequestParam String name,
@@ -44,7 +41,6 @@ public class GetController {
             @RequestParam String organization) {
         return name + " " + email + " " + organization;
     }
-     */
 
     //@RequestParam과 Map을 조합한 GET 메서드 구현
     @GetMapping(value = "request2")
