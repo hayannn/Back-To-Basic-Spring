e30e0ac5414eb3ae60371417e8f997b6a6601551
hayannn <dlgkdis801@naver.com>
2024-06-19 15:13:21 +0900

    feat: @RequestBody와 Map을 활용한 PUT 메서드 구현

--- src/main/java/com/springboot/api/controller/PutController.java
+++ src/main/java/com/springboot/api/controller/PutController.java
@@ -1,9 +1,26 @@
 package com.springboot.api.controller;
 
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/put-api")
 public class PutController {
+
+    //@RequestBody와 Map을 활용한 PUT 메서드 구현
+    @PutMapping(value = "/member")
+    public String postMemver(@RequestBody Map<String, Object> putData) {
+        StringBuilder sb = new StringBuilder();
+
+        putData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
 }
