7d65d09425c650102a2530e455da51ce449d60be
hayannn <dlgkdis801@naver.com>
2024-06-19 15:25:21 +0900

    feat: DTO 객체를 활용한 PUT 메서드 구현

--- src/main/java/com/springboot/api/controller/PutController.java
+++ src/main/java/com/springboot/api/controller/PutController.java
@@ -1,5 +1,6 @@
 package com.springboot.api.controller;
 
+import com.springboot.api.dto.MemberDTO;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,18 @@ public class PutController {
         });
 
         return sb.toString();
+    }
+
+    //DTO 객체를 활용한 PUT 메서드 구현 - String 리턴
+    @PutMapping(value = "/member1")
+    public String postMemberDTO1(@RequestBody MemberDTO memberDTO){
+        return memberDTO.toString();
+    }
+
+    //DTO 객체를 활용한 PUT 메서드 구현 - memberDTO 리턴
+    @PutMapping(value = "/member2")
+    public MemberDTO postMemberDTO2(@RequestBody MemberDTO memberDTO){
+        return memberDTO;
     }
 
 }
