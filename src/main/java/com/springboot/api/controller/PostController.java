d9d2a38240b8490daeb0a66951bec1f72957e4c3
hayannn <dlgkdis801@naver.com>
2024-06-19 14:36:42 +0900

    feat: 컨트롤러 클래스에서 공통 URL 설정(예제 5.10)

--- src/main/java/com/springboot/api/controller/PostController.java
+++ src/main/java/com/springboot/api/controller/PostController.java
@@ -1,4 +1,12 @@
 package com.springboot.api.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/post-api")
+
+//컨트롤러 클래스에서 공통 URL 설정
 public class PostController {
+
 }
