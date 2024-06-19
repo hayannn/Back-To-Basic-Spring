37e82ae112460aabac358971f311cc65d00a1fae
hayannn <dlgkdis801@naver.com>
2024-06-19 15:34:14 +0900

    feat: ResponseEntity를 활용한 PUT 메서드 구현

--- src/main/java/com/springboot/api/controller/PutController.java
+++ src/main/java/com/springboot/api/controller/PutController.java
@@ -1,6 +1,8 @@
 package com.springboot.api.controller;
 
 import com.springboot.api.dto.MemberDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +36,14 @@ public class PutController {
     @PutMapping(value = "/member2")
     public MemberDTO postMemberDTO2(@RequestBody MemberDTO memberDTO){
         return memberDTO;
+    }
+
+    //ResponseEntity를 활용한 PUT 메서드 구현
+    @PutMapping(value = "/member3")
+    public ResponseEntity<MemberDTO> postMemberDTO3(@RequestBody MemberDTO memberDTO){
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(memberDTO);
     }
 
 }
