3a35d903c6660f84171bebaa7947265e8954977e
hayannn <dlgkdis801@naver.com>
2024-06-19 15:44:31 +0900

    feat: @RequestParam을 활용한 DELETE 메서드 구현

--- src/main/java/com/springboot/api/controller/DeleteController.java
+++ src/main/java/com/springboot/api/controller/DeleteController.java
@@ -1,9 +1,6 @@
 package com.springboot.api.controller;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/delete-api")
@@ -13,5 +10,11 @@ public class DeleteController {
     @DeleteMapping(value = "/{variable}")
     public String DeleteVariable(@PathVariable String variable){
         return variable;
+    }
+
+    //@RequestParam을 활용한 DELETE 메서드 구현
+    @DeleteMapping(value = "request1")
+    public String getRequestParam1(@RequestParam String email){
+        return "e-mail : " + email;
     }
 }
