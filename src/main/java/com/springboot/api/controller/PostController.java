a779e7ab8a99f1e13eaa2a98e234e89f218a3337
hayannn <dlgkdis801@naver.com>
2024-06-19 14:54:07 +0900

    feat: @RequestBody를 활용한 POST 메서드 구현

--- src/main/java/com/springboot/api/controller/PostController.java
+++ src/main/java/com/springboot/api/controller/PostController.java
@@ -1,8 +1,8 @@
 package com.springboot.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/post-api")
@@ -14,5 +14,20 @@ public class PostController {
     @RequestMapping(value = "/domain", method = RequestMethod.POST)
     public String postExample() {
         return "Hello Post API";
+    }
+
+    //@RequestBody를 활용한 POST 메서드 구현
+    @PostMapping("member")
+    public String postMember(
+            @RequestBody Map<String, String> postData
+    ){
+        StringBuilder sb = new StringBuilder();
+        postData.entrySet().forEach(
+                map -> {
+                    sb.append(map.getKey() + " : " + map.getValue() + "\n");
+                }
+        );
+        return sb.toString();
+
     }
 }
