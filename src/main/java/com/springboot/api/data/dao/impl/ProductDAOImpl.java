c01e0d604ca727702d3374a7ed5c95a5c6d4ce18
hayannn <dlgkdis801@naver.com>
2024-06-21 17:18:57 +0900

    feat: updateProductName() 메서드 구현

--- src/main/java/com/springboot/api/data/dao/impl/ProductDAOImpl.java
+++ src/main/java/com/springboot/api/data/dao/impl/ProductDAOImpl.java
@@ -6,6 +6,9 @@ import com.springboot.api.data.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Component
 public class ProductDAOImpl implements ProductDAO {
 
@@ -30,9 +33,24 @@ public class ProductDAOImpl implements ProductDAO {
         return selectProduct;
     }
 
+    //업데이트
     @Override
     public Product updateProductName(Long number, String name) throws Exception {
-        return null;
+        Optional<Product> selectedProduct = productRepository.findById(number);
+
+        Product updatedProduct;
+        if(selectedProduct.isPresent()) {
+            Product product = selectedProduct.get();
+
+            product.setName(name);
+            product.setUpdatedAt(LocalDateTime.now());
+
+            updatedProduct = productRepository.save(product);
+        } else {
+            throw new Exception();
+        }
+
+        return updatedProduct;
     }
 
     @Override
