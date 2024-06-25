01171b9bd77053ef3269f5f656573fb222190abb
hayannn <dlgkdis801@naver.com>
2024-06-25 17:25:59 +0900

    chore: 프로젝트 진행을 위해 Lombok 다시 적용

--- src/main/java/com/springboot/api/data/entity/Product.java
+++ src/main/java/com/springboot/api/data/entity/Product.java
@@ -1,9 +1,17 @@
 package com.springboot.api.data.entity;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString(exclude = "name")
 @Table(name = "product")
 public class Product {
 
@@ -23,115 +31,4 @@ public class Product {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    public Product(Long number, String name, Integer price, Integer stock, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        this.number = number;
-        this.name = name;
-        this.price = price;
-        this.stock = stock;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
-
-    public Product() {
-    }
-
-    public Long getNumber() {
-        return this.number;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public Integer getPrice() {
-        return this.price;
-    }
-
-    public Integer getStock() {
-        return this.stock;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return this.createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return this.updatedAt;
-    }
-
-    public void setNumber(Long number) {
-        this.number = number;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-    public void setStock(Integer stock) {
-        this.stock = stock;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public boolean equals(final Object o) {
-        if (o == this) return true;
-        if (!(o instanceof Product)) return false;
-        final Product other = (Product) o;
-        if (!other.canEqual((Object) this)) return false;
-        final Object this$number = this.getNumber();
-        final Object other$number = other.getNumber();
-        if (this$number == null ? other$number != null : !this$number.equals(other$number)) return false;
-        final Object this$name = this.getName();
-        final Object other$name = other.getName();
-        if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
-        final Object this$price = this.getPrice();
-        final Object other$price = other.getPrice();
-        if (this$price == null ? other$price != null : !this$price.equals(other$price)) return false;
-        final Object this$stock = this.getStock();
-        final Object other$stock = other.getStock();
-        if (this$stock == null ? other$stock != null : !this$stock.equals(other$stock)) return false;
-        final Object this$createdAt = this.getCreatedAt();
-        final Object other$createdAt = other.getCreatedAt();
-        if (this$createdAt == null ? other$createdAt != null : !this$createdAt.equals(other$createdAt)) return false;
-        final Object this$updatedAt = this.getUpdatedAt();
-        final Object other$updatedAt = other.getUpdatedAt();
-        if (this$updatedAt == null ? other$updatedAt != null : !this$updatedAt.equals(other$updatedAt)) return false;
-        return true;
-    }
-
-    protected boolean canEqual(final Object other) {
-        return other instanceof Product;
-    }
-
-    public int hashCode() {
-        final int PRIME = 59;
-        int result = 1;
-        final Object $number = this.getNumber();
-        result = result * PRIME + ($number == null ? 43 : $number.hashCode());
-        final Object $name = this.getName();
-        result = result * PRIME + ($name == null ? 43 : $name.hashCode());
-        final Object $price = this.getPrice();
-        result = result * PRIME + ($price == null ? 43 : $price.hashCode());
-        final Object $stock = this.getStock();
-        result = result * PRIME + ($stock == null ? 43 : $stock.hashCode());
-        final Object $createdAt = this.getCreatedAt();
-        result = result * PRIME + ($createdAt == null ? 43 : $createdAt.hashCode());
-        final Object $updatedAt = this.getUpdatedAt();
-        result = result * PRIME + ($updatedAt == null ? 43 : $updatedAt.hashCode());
-        return result;
-    }
-
-    public String toString() {
-        return "Product(number=" + this.getNumber() + ", price=" + this.getPrice() + ", stock=" + this.getStock() + ", createdAt=" + this.getCreatedAt() + ", updatedAt=" + this.getUpdatedAt() + ")";
-    }
 }
