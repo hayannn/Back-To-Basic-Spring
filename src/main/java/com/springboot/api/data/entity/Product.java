4b873517ade1cebed388b78e1502be729ed30001
hayannn <dlgkdis801@naver.com>
2024-06-26 16:55:57 +0900

    chore: @EqualsAndHashCode Delombok 수행 시 코드

--- src/main/java/com/springboot/api/data/entity/Product.java
+++ src/main/java/com/springboot/api/data/entity/Product.java
@@ -10,7 +10,6 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
 @ToString(exclude = "name")
 @Table(name = "product")
 public class Product {
@@ -31,4 +30,51 @@ public class Product {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Product)) return false;
+        final Product other = (Product) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$number = this.getNumber();
+        final Object other$number = other.getNumber();
+        if (this$number == null ? other$number != null : !this$number.equals(other$number)) return false;
+        final Object this$name = this.getName();
+        final Object other$name = other.getName();
+        if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
+        final Object this$price = this.getPrice();
+        final Object other$price = other.getPrice();
+        if (this$price == null ? other$price != null : !this$price.equals(other$price)) return false;
+        final Object this$stock = this.getStock();
+        final Object other$stock = other.getStock();
+        if (this$stock == null ? other$stock != null : !this$stock.equals(other$stock)) return false;
+        final Object this$createdAt = this.getCreatedAt();
+        final Object other$createdAt = other.getCreatedAt();
+        if (this$createdAt == null ? other$createdAt != null : !this$createdAt.equals(other$createdAt)) return false;
+        final Object this$updatedAt = this.getUpdatedAt();
+        final Object other$updatedAt = other.getUpdatedAt();
+        if (this$updatedAt == null ? other$updatedAt != null : !this$updatedAt.equals(other$updatedAt)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Product;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $number = this.getNumber();
+        result = result * PRIME + ($number == null ? 43 : $number.hashCode());
+        final Object $name = this.getName();
+        result = result * PRIME + ($name == null ? 43 : $name.hashCode());
+        final Object $price = this.getPrice();
+        result = result * PRIME + ($price == null ? 43 : $price.hashCode());
+        final Object $stock = this.getStock();
+        result = result * PRIME + ($stock == null ? 43 : $stock.hashCode());
+        final Object $createdAt = this.getCreatedAt();
+        result = result * PRIME + ($createdAt == null ? 43 : $createdAt.hashCode());
+        final Object $updatedAt = this.getUpdatedAt();
+        result = result * PRIME + ($updatedAt == null ? 43 : $updatedAt.hashCode());
+        return result;
+    }
 }
